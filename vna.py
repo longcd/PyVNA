9389eb68cb2719b5dba9750e72c1d0674b73341a
longcd <longchangda@qq.com>
2022-09-19 07:14:33 +0800

    fix bug

--- vna.py
+++ vna.py
@@ -19,15 +19,15 @@ class VNA:
     ####################
     def active_channel(self, ch: int = 1) -> None:
         """Specifies selected channel (Ch) as the active channel."""
-        self._instr.write(":DISP:WIND{ch}:ACT".format(ch))
+        self._instr.write(f":DISP:WIND{ch}:ACT")
 
     def active_trace(self, tr: int, ch: int = 1) -> None:
         """Sets/gets the selected trace(Tr) of selected channel(Ch) to the active trace."""
-        self._instr.write(":CALC{ch}:PAR{tr}:SEL".format(ch, tr))
+        self._instr.write(f":CALC{ch}:PAR{tr}:SEL")
 
     def trace_number(self, num: int, ch: int = 1) -> None:
         """Sets/gets the number of traces of selected channel(Ch)."""
-        self._instr.write(":CALC{ch}:PAR:COUN {num}".format(ch, num))
+        self._instr.write(f":CALC{ch}:PAR:COUN {num}")
 
     ####################
     # Frequency
@@ -42,9 +42,9 @@ class VNA:
             Set the start value if freq is a float number, the unit is Hz(hertz).
         """
         if isinstance(freq, str) and freq == "?":
-            return self._instr.query(":SENS{ch}:FREQ:STAR?".format(ch))
+            return self._instr.query(f":SENS{ch}:FREQ:STAR?")
         else:
-            return self._instr.write(":SENS{ch}:FREQ:STAR {freq}".format(ch, freq))
+            return self._instr.write(f":SENS{ch}:FREQ:STAR {freq}")
 
     def freq_stop(self, freq: Union[float, str], ch: int = 1) -> str:
         """Sets/gets the stop value of the sweep range of selected channel(Ch).
@@ -56,9 +56,9 @@ class VNA:
             Set the stop value if freq is a float number, the unit is Hz(hertz).
         """
         if isinstance(freq, str) and freq == "?":
-            return self._instr.query(":SENS{ch}:FREQ:STAR?".format(ch))
+            return self._instr.query(f":SENS{ch}:FREQ:STAR?")
         else:
-            return self._instr.write(":SENS{ch}:FREQ:STOP {freq}".format(ch, freq))
+            return self._instr.write(f":SENS{ch}:FREQ:STOP {freq}")
 
     def freq_center(self, freq: Union[float, str], ch: int = 1) -> str:
         """Sets/gets the center value of the sweep range of selected channel(Ch).
@@ -70,9 +70,9 @@ class VNA:
             Set the center value if freq is a float number, the unit is Hz(hertz).
         """
         if isinstance(freq, str) and freq == "?":
-            return self._instr.query(":SENS{ch}:FREQ:CENT?".format(ch))
+            return self._instr.query(f":SENS{ch}:FREQ:CENT?")
         else:
-            return self._instr.write(":SENS{ch}:FREQ:CENT {freq}".format(ch, freq))
+            return self._instr.write(f":SENS{ch}:FREQ:CENT {freq}")
 
     def freq_span(self, freq: Union[float, str], ch: int = 1) -> str:
         """Sets/gets the span value of the sweep range of selected channel(Ch).
@@ -84,9 +84,9 @@ class VNA:
             Set the span value if freq is a float number, the unit is Hz(hertz).
         """
         if isinstance(freq, str) and freq == "?":
-            return self._instr.query(":SENS1:FREQ:SPAN?".format(ch))
+            return self._instr.query(f":SENS1:FREQ:SPAN?")
         else:
-            return self._instr.write(":SENS{ch}:FREQ:SPAN {freq}".format(ch, freq))
+            return self._instr.write(f":SENS{ch}:FREQ:SPAN {freq}")
 
     ####################
     # Sweep Setup
@@ -101,9 +101,9 @@ class VNA:
             Set the power level if power is a float number, the unit is dBm.
         """
         if isinstance(power, str) and power == "?":
-            return self._instr.query(":SOUR{ch}:POW?".format(ch))
+            return self._instr.query(f":SOUR{ch}:POW?")
         else:
-            return self._instr.write(":SOUR{ch}:POW {power}".format(ch, power))
+            return self._instr.write(f":SOUR{ch}:POW {power}")
 
     def points(self, point: Union[int, str], ch: int = 1) -> str:
         """Sets/gets the number of measurement points of selected channel(Ch).
@@ -115,9 +115,9 @@ class VNA:
             Set the points if point is a int number.
         """
         if isinstance(point, str) and point == "?":
-            return self._instr.query(":SENS{ch}:SWE:POIN?".format(ch))
+            return self._instr.query(f":SENS{ch}:SWE:POIN?")
         else:
-            return self._instr.write(":SENS{ch}:SWE:POIN {point}".format(ch, point))
+            return self._instr.write(f":SENS{ch}:SWE:POIN {point}")
 
     def sweep_type(self, typ: str, ch: int = 1) -> str:
         """Sets/gets the sweep type of selected channel(Ch).
@@ -133,11 +133,11 @@ class VNA:
             - "POWer": Sets the sweep type to the power sweep.
         """
         if typ == "?":
-            return self._instr.query(":SENS{ch}:SWE:TYPE?".format(ch))
+            return self._instr.query(f":SENS{ch}:SWE:TYPE?")
         else:
-            return self._instr.write(":SENS{ch}:SWE:TYPE {typ}".format(ch, typ))
+            return self._instr.write(f":SENS{ch}:SWE:TYPE {typ}")
 
-    def segm_data(self, data: str, ch: int = 1) -> str:
+    def segm_data(self, data: str, ch=1) -> str:
         """Creates the segment sweep table of selected channel(Ch).
 
         Parameters
@@ -146,9 +146,9 @@ class VNA:
             See: https://rfmw.em.keysight.com/wireless/helpfiles/e5071c/programming/command_reference/sense/scpi_sense_ch_segment_data.htm
         """
         if data == "?":
-            return self._instr.query(":SENS{ch}:SEGM:DATA?".format(ch))
+            return self._instr.query(f":SENS{ch}:SEGM:DATA?")
         else:
-            return self._instr.write(":SENS{ch}:SEGM:DATA {data}".format(ch, data))
+            return self._instr.write(f":SENS{ch}:SEGM:DATA {data}")
 
     ####################
     # RESPONSE
@@ -163,9 +163,9 @@ class VNA:
             Set the IF bandwidth if bw is a float number.
         """
         if isinstance(bw, str) and bw == "?":
-            return self._instr.query(":SENS{ch}:BAND?".format(ch))
+            return self._instr.query(f":SENS{ch}:BAND?")
         else:
-            return self._instr.write(":SENS{ch}:BAND {bw}".format(ch, bw))
+            return self._instr.write(f":SENS{ch}:BAND {bw}")
 
     def trace_num(self, num: Union[int, str], ch: int = 1) -> str:
         """Sets/gets the number of traces of selected channel(Ch).
@@ -177,9 +177,9 @@ class VNA:
             Set the number of traces if num is a int number.
         """
         if isinstance(num, str) and num == "?":
-            return self._instr.query(":CALC{ch}:PAR:COUN?".format(ch))
+            return self._instr.query(f":CALC{ch}:PAR:COUN?")
         else:
-            return self._instr.write(":CALC{ch}:PAR:COUN {num}".format(ch, num))
+            return self._instr.write(f":CALC{ch}:PAR:COUN {num}")
 
     def window_layout(self, layout: str, ch: int = 1) -> str:
         """Sets/gets the graph layout of selected channel(Ch).
@@ -190,9 +190,9 @@ class VNA:
             See: https://rfmw.em.keysight.com/wireless/helpfiles/e5071c/programming/command_reference/display/scpi_display_window_ch_split.htm
         """
         if layout == "?":
-            return self._instr.query(":DISP:WIND:SPL?")
+            return self._instr.query(":DISP:WIND{ch}:SPL?")
         else:
-            return self._instr.write(":DISP:WIND:SPL {}".format(layout))
+            return self._instr.write(f":DISP:WIND{ch}:SPL {layout}")
 
     def parameter(self, para: str, tr: int, ch: int = 1) -> str:
         """Sets/gets the measurement parameter of the selected trace(Tr), for the selected channel(Ch).
@@ -203,11 +203,9 @@ class VNA:
             See: https://rfmw.em.keysight.com/wireless/helpfiles/e5071c/programming/command_reference/calculate/scpi_calculate_ch_parameter_tr_define.htm
         """
         if para == "?":
-            return self._instr.query(":CALC{ch}:PAR{tr}:DEF?".format(ch, tr))
+            return self._instr.query(f":CALC{ch}:PAR{tr}:DEF?")
         else:
-            return self._instr.write(
-                ":CALC{ch}:PAR{tr}:DEF {para}".format(ch, tr, para)
-            )
+            return self._instr.write(f":CALC{ch}:PAR{tr}:DEF {para}")
 
     def format(self, form: str, tr: int, ch: int = 1) -> str:
         """Sets/gets the data format of the active trace of selected channel(Ch).
@@ -218,11 +216,9 @@ class VNA:
             See: https://rfmw.em.keysight.com/wireless/helpfiles/e5071c/programming/command_reference/calculate/scpi_calculate_ch_selected_format.htm
         """
         if form == "?":
-            return self._instr.query(":CALC{ch}:TRAC{tr}:FORM?".format(ch, tr))
+            return self._instr.query(f":CALC{ch}:TRAC{tr}:FORM?")
         else:
-            return self._instr.write(
-                ":CALC{ch}:TRAC{tr}:FORM {form}".format(ch, tr, form)
-            )
+            return self._instr.write(f":CALC{ch}:TRAC{tr}:FORM {form}")
 
     ####################
     # Calibration
@@ -237,9 +233,9 @@ class VNA:
             Sets the calibration kit if kit is a integer.
         """
         if kit == "?":
-            return self._instr.query(":SENS{ch}:CORR:COLL:CKIT?".format(ch))
+            return self._instr.query(f":SENS{ch}:CORR:COLL:CKIT?")
         else:
-            return self._instr.write(":SENS{ch}:CORR:COLL:CKIT {kit}".format(ch, kit))
+            return self._instr.write(f":SENS{ch}:CORR:COLL:CKIT {kit}")
 
     def cal_meth(self, ports: str, ch: int = 1) -> str:
         """Sets the calibration type to the full 2-port calibration between the specified 2 ports, for the selected channel(Ch).
@@ -249,9 +245,7 @@ class VNA:
         ports: str
             Specifies the port for full 2-port calibration. eg. 1,2
         """
-        return self._instr.write(
-            ":SENS{ch}:CORR:COLL:METH:SOLT2 {ports}".format(ch, ports)
-        )
+        return self._instr.write(f":SENS{ch}:CORR:COLL:METH:SOLT2 {ports}")
 
     def cal_open(self, port: int, ch: int = 1) -> None:
         """Measures the calibration data of the open standard for the specified port, for the selected channel(Ch).
@@ -261,7 +255,7 @@ class VNA:
         port: int
             the specified port
         """
-        self._instr.write(":SENS{ch}:CORR:COLL:OPEN {port}".format(ch, port))
+        self._instr.write(f":SENS{ch}:CORR:COLL:OPEN {port}")
         self._instr.query("*OPC?")
 
     def cal_shor(self, port: int, ch: int = 1) -> None:
@@ -272,7 +266,7 @@ class VNA:
         port: int
             the specified port
         """
-        self._instr.write(":SENS{ch}:CORR:COLL:SHOR {port}".format(ch, port))
+        self._instr.write(f":SENS{ch}:CORR:COLL:SHOR {port}")
         self._instr.query("*OPC?")
 
     def cal_load(self, port: int, ch: int = 1) -> None:
@@ -283,25 +277,21 @@ class VNA:
         port: int
             the specified port
         """
-        self._instr.write(":SENS{ch}:CORR:COLL:LOAD {port}".format(ch, port))
+        self._instr.write(f":SENS{ch}:CORR:COLL:LOAD {port}")
         self._instr.query("*OPC?")
 
     def cal(self, typ: str, port: int, ch: int = 1) -> None:
-        self._instr.write(":SENS{ch}:CORR:COLL:{typ} {port}".format(ch, typ, port))
+        self._instr.write(f":SENS{ch}:CORR:COLL:{typ} {port}")
         self._instr.query("*OPC?")
 
     def cal_thru(self, port1: int, port2: int, ch: int = 1) -> None:
         """Measures the calibration data of the Thru standard from the specified stimulus port to the specified response port, for the selected channel(Ch)."""
-        self._instr.write(
-            ":SENS{ch}:CORR:COLL:THRU {port1},{port2}".format(ch, port1, port2)
-        )
-        self._instr.write(
-            ":SENS{ch}:CORR:COLL:THRU {port2},{port1}".format(ch, port1, port2)
-        )
+        self._instr.write(f":SENS{ch}:CORR:COLL:THRU {port1},{port2}")
+        self._instr.write(f":SENS{ch}:CORR:COLL:THRU {port2},{port1}")
         self._instr.query("*OPC?")
 
     def cal_done(self, ch: int = 1) -> None:
-        self._instr.write(":SENS{ch}:CORR:COLL:SAVE".format(ch))
+        self._instr.write(f":SENS{ch}:CORR:COLL:SAVE")
 
     ####################
     # Save/Recall
@@ -313,10 +303,10 @@ class VNA:
         if stype == "?":
             return self._instr.query(":MMEM:STOR:STYP?")
         else:
-            return self._instr.write(":MMEM:STOR:STYP {}".format(stype))
+            return self._instr.write(f":MMEM:STOR:STYP {stype}")
 
     def mdir(self, folder: str) -> None:
-        self._instr.write(":MMEM:MDIR '{folder}'".format(folder))
+        self._instr.write(f":MMEM:MDIR '{folder}'")
 
     def stor_stat(self, file: str) -> None:
         """Saves the instrument state into a file(file with the .sta extension).
@@ -327,7 +317,7 @@ class VNA:
             File name to save the instrument state (extension ".sta")
             eg. 'D:/FILTERS/test.sta'
         """
-        self._instr.write(":MMEM:STOR '{file}'".format(file))
+        self._instr.write(f":MMEM:STOR '{file}'")
 
     def load_stat(self, file: str) -> None:
         """This command recalls the specified instrument state file.
@@ -338,29 +328,29 @@ class VNA:
             File name of instrument state (extension ".sta")
             eg. 'D:/FILTERS/test.sta'
         """
-        self._instr.write(":MMEM:LOAD '{file}'".format(file))
+        self._instr.write(f":MMEM:LOAD '{file}'")
 
     ####################
     # CALCULATE
     ####################
     def calc_sdat(self, tr: int, ch: int = 1) -> str:
         """Gets the corrected data array, for the active trace of selected channel(Ch)."""
-        return self._instr.write(":CALC{ch}:TRAC{tr}:DATA:SDAT?".format(ch, tr))
+        return self._instr.query(f":CALC{ch}:TRAC{tr}:DATA:SDAT?")
 
     def calc_fdat(self, tr: int, ch: int = 1) -> str:
         """Gets the formatted data array, for the active trace of selected channel(Ch)."""
-        return self._instr.write(":CALC{ch}:TRAC{tr}:DATA:FDAT?".format(ch, tr))
+        return self._instr.query(f":CALC{ch}:TRAC{tr}:DATA:FDAT?")
 
     def calc_mfd(self, trs: str, ch: int = 1) -> str:
         """Gets the formatted data array of multiple traces of the selected channel(Ch)."""
-        return self._instr.write(":CALC{ch}:TRAC:DATA:MFD? '{trs}'".format(ch, trs))
+        return self._instr.query(f":CALC{ch}:TRAC:DATA:MFD? '{trs}'")
 
     def init_cont(self, status: str, ch: int = 1) -> None:
         """Turns ON/OFF the continuous initiation mode of selected channel (Ch) in the trigger system."""
-        self._instr.write(":INIT{ch}:CONT {status}".format(ch, status))
+        self._instr.write(f":INIT{ch}:CONT {status}")
 
     def trigger(self, ch: int = 1) -> None:
-        self._instr.write(":INIT{ch}".format(ch))
+        self._instr.write(f":INIT{ch}")
 
     # Limit Test
     def limit_display(self, state: str, tr: int, ch: int = 1) -> str:
@@ -372,11 +362,9 @@ class VNA:
             ON/OFF
         """
         if state == "?":
-            return self._instr.query(":CALC{ch}:TRAC{tr}:LIM:DISP?".format(ch, tr))
+            return self._instr.query(f":CALC{ch}:TRAC{tr}:LIM:DISP?")
         else:
-            return self._instr.write(
-                ":CALC{ch}:TRAC{tr}:LIM:DISP {state}".format(ch, tr, state)
-            )
+            return self._instr.write(f":CALC{ch}:TRAC{tr}:LIM:DISP {state}")
 
     def limit_data(self, data: str, tr: int, ch: int = 1) -> str:
         """Sets/gets the limit table for the limit test, for the active trace of selected channel(Ch).
@@ -384,11 +372,9 @@ class VNA:
         See: https://rfmw.em.keysight.com/wireless/helpfiles/e5071c/programming/command_reference/calculate/scpi_calculate_ch_selected_limit_data.htm
         """
         if data == "?":
-            return self._instr.query(":CALC{ch}:TRAC{tr}:LIM:DATA?".format(ch, tr))
+            return self._instr.query(f":CALC{ch}:TRAC{tr}:LIM:DATA?")
         else:
-            return self._instr.write(
-                ":CALC{ch}:TRAC{tr}:LIM:DATA {data}".format(ch, tr, data)
-            )
+            return self._instr.write(f":CALC{ch}:TRAC{tr}:LIM:DATA {data}")
 
     # IEEE4882
     def preset(self) -> None:
